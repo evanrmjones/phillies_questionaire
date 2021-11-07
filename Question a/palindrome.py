2eed77aa05f05c943b2fca0448e4144d41297a88
Evan Jones <ermjones@email.unc.edu>
2021-11-06 20:29:39 -0400

    final 3

---
+++ Question a/palindrome.py
@@ -0,0 +1,24 @@
+def is_palindrone(s):
+    """Old function"""
+    r=""
+    for c in s:
+        r = c +r
+    for x in range(0, len(s)):
+        if s[x] == r[x]:
+            x = True
+        else:
+            return False
+    return x
+
+def better_is_palindrome(s):
+    """Better function to check if a word is a palinfrome."""
+    r = ""
+    for c in s: 
+        r = c + r 
+    if s == r: 
+        return True 
+    else: 
+        return False
+
+
+print(better_is_palindrome("racecar"))
